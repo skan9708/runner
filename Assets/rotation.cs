adf3cd14c2238e5476611306f34cd57047bea0f0
djeymac <djey9538@gmail.com>
2016-04-12 18:28:21 +0900

    init : scene change example

---
+++ Assets/rotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(3,2,1) * 50 * Time.deltaTime);
+	}
+}
---
+++ Assets/sceneChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneChanger: MonoBehaviour {
+
+	int sum = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (SceneManager.GetActiveScene().name == "logo")
+        {
+            if (100 == sum)
+            {
+                ToScene("main");
+            }
+            sum += 1;
+        }
+	}
+
+    public void ToScene(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+    }
+}
