7674ea82ead0baca03b5020e8c45cc398e562b25
djeymac <djey9538@gmail.com>
2016-04-14 17:43:46 +0900

    CoinSpawner

---
+++ Assets/scripts/CoinSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject Coin;
+    public int duration=100;
+    public int sum=0;
+
+    void Update()
+    {
+        if (sum == duration)
+        {
+            sum = 0;
+            Spawn();
+            return;
+        }
+        sum += 1;
+    }
+
+    void Spawn()
+    {
+        GameObject coin = (GameObject)Instantiate(Coin);
+        // 위치
+        Vector2 spawnPosition = transform.position;
+        coin.transform.position = spawnPosition;
+        // 속도
+        Rigidbody2D rb = coin.GetComponent<Rigidbody2D>();
+        Vector2 v = rb.velocity;
+        v.x -= 10;
+        rb.velocity = v;
+    }
+}
