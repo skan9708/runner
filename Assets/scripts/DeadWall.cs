d8f05ef81a8f20640ff46ad48f286aefcc483d68
djeymac <djey9538@gmail.com>
2016-04-14 20:05:13 +0900

    coin DeadWall

---
+++ Assets/scripts/DeadWall.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadWall : MonoBehaviour {
+
+    void OnTriggerEnter2D (Collider2D other)
+	{
+        // 단순히 충돌한 대상을 없앤다.
+		Destroy(other.gameObject);
+	}
+}
